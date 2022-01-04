0d698ebcad88c418e9c5fbb9c632bafd1ef84497
brigi <mannadsgn@gmail.com>
2022-01-04 15:19:32 +0100

    ADD LOGIN

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
+          <Route
+            path="login"
+            element={
+              <>
+                <Header />
+                <Login />
+              </>
+            }
+          />
           <Route
             path="/"
             element={
---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login__contetntbox">
+        <div className="login__CTAone">
+          <img src="/images/cta-logo-one.svg" alt="ctalogo" />
+        </div>
+        <button className="login__signup">GET ALL THERE</button>
+        <div className="login__description">fdgdsgfhgfjhkuzouipoiői</div>
+        <div className="login__CTAtwo">
+          <img src="/images/cta-logo-two.png" alt="ctalogotwo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
