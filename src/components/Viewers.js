b471f40e9bef9f590dfe8c3c3bc65cfa5109dcb9
brigi <mannadsgn@gmail.com>
2022-01-07 10:42:51 +0100

    add wievers videos

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,19 +5,44 @@ const Viewers = () => {
   return (
     <div className="viewers">
       <div className="image__container">
-        <img src="/images/viewers-disney.png" alt="" />
+        {" "}
+        <img src="/images/viewers-disney.png" alt="disney" />
+        <video autoPlay loop playsInline>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
       </div>
+
       <div className="image__container">
+        {" "}
         <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay loop playsInline>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
       </div>
+
       <div className="image__container">
+        {" "}
         <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay loop playsInline>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
       </div>
       <div className="image__container">
+        {" "}
         <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay loop playsInline>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
       </div>
       <div className="image__container">
+        {" "}
         <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay loop playsInline>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
       </div>
     </div>
   );
